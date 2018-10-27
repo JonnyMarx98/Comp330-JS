c97a4fcce2d6f53c78c1ab1d609a6da3489ed0bd
Jonny <jonnymarx98@gmail.com>
2018-10-27 14:04:23 +0100

    shoot in 4 directions and attempted shoot in mouse direction

--- bullet.js
+++ bullet.js
@@ -5,9 +5,57 @@ class bullet {
 
         this.bulletX = x;
         this.bulletY = y;
+        this.bulletDirection;
+        // this.Up = false;
+        // this.Right = false;
+        // this.p = 0;
     }
     Update() {
-        this.bulletX += BULLET_SPEED;
+        if (this.bulletDirection == 0) this.bulletX -= BULLET_SPEED;
+        if (this.bulletDirection == 1) this.bulletY -= BULLET_SPEED;
+        if (this.bulletDirection == 2) this.bulletX += BULLET_SPEED;
+        if (this.bulletDirection == 3) this.bulletY += BULLET_SPEED;
+
+        // if (this.p < 1){
+        //     if (this.bulletX < this.directionX || this.Right == true) {
+        //         this.bulletX += BULLET_SPEED;
+        //         this.Right = true;
+        //     }
+        //     if (this.bulletX > this.directionX || this.Right == false) {
+        //         this.bulletX -= BULLET_SPEED;
+        //         this.Right = false;
+        //     }
+        //     if (this.bulletY < this.directionY || this.Up == false) {
+        //         this.bulletY += BULLET_SPEED;
+        //         this.Up = false;
+        //     }
+        //
+        //     if (this.bulletY > this.directionY || this.Up == true) {
+        //         this.bulletY -= BULLET_SPEED;
+        //         this.Up = true;
+        //     }
+        //     this.p++;
+        // }
+        // else {
+        //     if (this.Right == true) {
+        //         this.bulletX += BULLET_SPEED;
+        //         this.Right = true;
+        //     }
+        //     if (this.Right == false) {
+        //         this.bulletX -= BULLET_SPEED;
+        //         this.Right = false;
+        //     }
+        //     if (this.Up == false) {
+        //         this.bulletY += BULLET_SPEED;
+        //         this.Up = false;
+        //     }
+        //
+        //     if (this.Up == true) {
+        //         this.bulletY -= BULLET_SPEED;
+        //         this.Up = true;
+        //     }
+        // }
+
 
     }
     Draw() {
@@ -19,8 +67,8 @@ class bullet {
         for (o=0; o<enemies.length; o++){
             // enemies[i].enemyX;
             if(this.bulletX + BULLET_WIDTH > enemies[o].enemyX &&
-                this.bulletX - BULLET_WIDTH < enemies[o].enemyX &&
-                this.bulletY - BULLET_HEIGHT < enemies[o].enemyY &&
+                this.bulletX - ENEMY_WIDTH < enemies[o].enemyX &&
+                this.bulletY - ENEMY_HEIGHT < enemies[o].enemyY &&
                 this.bulletY + BULLET_HEIGHT > enemies[o].enemyY) {
                 enemies[o].Reset();
                 if (!upgradeActive){
